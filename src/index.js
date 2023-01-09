da036591d6c30974058bb37cfc220f22b579f7a5
Sérgio Rebelo <sergiorebelo@me.com>
2023-01-09 18:19:44 +0100

    setup express sever

--- src/index.js
+++ src/index.js
@@ -9,12 +9,20 @@ const PORT = process.env.PORT || "8000";
 
 APP.use(CORS());
 
-/*APP.use(EXPRESS.json());
+APP.use(EXPRESS.json());
 APP.use(EXPRESS.urlencoded());
-*/
+
 APP.use(EXPRESS.static('public'));
 
 APP.listen(PORT, () => {
     console.log(`👂application running at port ${PORT}`);
 });
 
+APP.get("/text/:input", (req, res) => {
+   console.log(`${req.params.input}`);
+   res.status(200).send(req.params.input);
+});
+
+APP.get("*", (req, res) => {
+    res.status(404).send(`page not found!`);
+});
