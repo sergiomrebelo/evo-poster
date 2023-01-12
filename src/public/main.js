2a11362b69a761e7a32e9cff23e5caad73238dbe
Sérgio Rebelo <sergiorebelo@me.com>
2023-01-12 18:44:48 +0100

    input interface

---
+++ src/public/main.js
@@ -0,0 +1,7 @@
+const bootstrap = require('bootstrap');
+const style = require('./main.css');
+// const bootstrapStyle = require('bootstrap/dist/css/bootstrap.css');
+
+// TODO: BUILD
+// const bootstrapStyle = require('bootstrap/dist/css/bootstrap.css');
+// const mainStyle = require('./main.css');
