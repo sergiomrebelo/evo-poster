f7a9bd2cf325791e18227e7bf923bf3222cc3c6e
Sérgio Rebelo <sergiorebelo@me.com>
2023-01-27 00:54:28 +0100

    feat(nlp) #9: ml-emotion-analysis module

--- src/nlp-utils/ml-emotion-analysis/ml-emotion-analysis.mjs
+++ src/nlp-utils/ml-emotion-analysis/ml-emotion-analysis.mjs
@@ -11,23 +11,26 @@
  */
 
 import {fileURLToPath} from "url";
-import {dirname} from "path";
+import {dirname, join} from "path";
 import Emoji from "node-emoji";
-import {availableLanguages} from "../nlp_utils.mjs";
+import {setup, isLangAvailable, translate} from "../translator/translator.mjs";
+import {NeuralNetwork} from "@nlpjs/neural";
+import {readFileSync} from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// params file
-const MIN_EMOTION_LEXICON = .5;
-const _MODEL = 'data/models/LST_AIT_2018_SemEval_2018_task_1_model_1624288181749.json';
+// params
+const _MODEL = 'data/LST_AIT_2018_SemEval_2018_task_1_model_1624288181749.json';
+const MIN_EMOTION_ML = .5;
 
-let config = false; // is library already configurated
+let _isConfig = false;
+const net = new NeuralNetwork();
 
 export const classification = async (txt, lang='en') => {
    // check if the library is config
    try {
-      _isConfig();
+      isConfig();
    } catch (e) {
       console.error(e);
    }
@@ -35,17 +38,15 @@ export const classification = async (txt, lang='en') => {
    const _rawTxt = txt;
    let _rawEnTxt = txt, _translateResults = txt;
 
-   txt = await rm(txt);
+   txt = await _rm(txt);
+   let displayTxt = txt;
 
-   // check if language is supported
-   // get mother language
-   lang = lang.split('-')[0];
-   lang = !availableLanguages.includes(lang) ? `en` : lang;
+   lang = isLangAvailable(lang);
 
    // translate text
    if (lang !== 'en') {
       try {
-         _translateResults = await _translate(txt, lang);
+         _translateResults = await translate(txt, lang);
          _translateResults = JSON.parse(_translateResults);
          txt = _translateResults.translations[0].translation;
          _rawEnTxt = txt;
@@ -57,16 +58,108 @@ export const classification = async (txt, lang='en') => {
       }
    }
 
-   console.log ("txt", txt, lang);
+   // remove retweet data
+   const rtExp = /RT\s*@[^:]*:/gm;
+   const rtMatch = txt.match(rtExp);
+   let rtInfo = rtMatch !== null ? rtExp : []; // retweet data
+   rtInfo = rtInfo.length > 0 ? rtInfo : [];
+
+   // remove URLs (hardwired method)
+   const urlExp = /(https?:\/\/)(\s)*(www\.)?(\s)*((\w|\s)+\.)*([\w\-\s]+\/)*([\w\-]+)((\?)?[\w\s]*=\s*[\w&]*)*/gm;
+   const urlsMatch = txt.match(urlExp);
+   let urls = urlsMatch !== null ? urlsMatch : [];
+
+   // remove of mentions
+   const mentionExp = /\B@[a-z0-9_-]+/gi;
+   const mentionMatch = txt.match(mentionExp);
+   const mentions = mentionMatch !== null ? mentionMatch : [];
+   let mentionsNative = mentions;
+   if (lang !== 'en') {
+      const mentionsNativeMatch = _rawTxt.match(mentionExp, '');
+      mentionsNative = mentionsNativeMatch !== null ? mentionsNativeMatch : [];
+   }
+
+   // remove hashtags
+   const htgexp = /#[^\s!@#$%^&*()=+.\/,\[{\]};:'"?><]+/gi;
+   const htgMatch = txt.match(htgexp, '');
+   const hashtags = htgMatch !== null ? htgMatch : [];
+
+   // get emojis
+   let emojis = [];
+   Emoji.replace(txt, (e) => emojis.push(e), true);
+
+   let result = null, emotionalData = null, success = true;
+
+   try {
+      result = await _run(txt);
+      emotionalData = _mostPresentEmotion(result, MIN_EMOTION_ML);
+      displayTxt.trim();
+   } catch (err) {
+      success = false;
+   }
+
+   return {
+      success: success,
+      type: "classification",
+      emotions: {
+         data: emotionalData
+      },
+      text: displayTxt,
+      meta: {
+         lang: lang,
+         RTInfo: rtInfo,
+         urls: urls,
+         hashtags: hashtags,
+         mentions: {
+            mentions: mentionsNative,
+            working: mentions
+         },
+         emojis: emojis,
+      },
+      _rawTxt: _rawTxt,
+      _MIN_REG_EMOTION_THRESHOLD: MIN_EMOTION_ML,
+      _raw: {
+         _workingTxt: txt,
+         _text: _rawTxt,
+         _enText: _rawEnTxt,
+         _analysis: result,
+         _translateResults: _translateResults
+      }
+   }
 }
 
-const _isConfig = () => {
-   /*if (!config) {
+const isConfig = () => {
+   if (!_isConfig) {
       throw new Error(`⚠️ Nlp-utils is not configurated! ⚠️`);
-   }*/
+   }
 }
 
-const rm = async (rawTxt) => {
+export const config = async (key = null, serviceURL) => {
+   console.info (`👋`);
+   console.info(`Configuring ML-Emotion-Analysis (nlp-utils)`)
+   try {
+      //  setup classifier
+      const model = readFileSync(join(__dirname, _MODEL));
+      let classifier = JSON.parse(model); // load model on classifier
+      net.fromJSON(classifier);
+      console.info(`✅ classifier`)
+   } catch (err) {
+      console.error(`❌ classifier not loaded: ${err} ❌`);
+      return false;
+   }
+
+   return new Promise((resolve) => {
+      setup(key, serviceURL).then(() => {
+         console.info(`✅ translator `)
+         _isConfig = true;
+         resolve(true);
+      }).catch((err) => {
+         console.error(`❌ translator not set up at nlp-utils (ML-Emotion-analysis)  ${err} ❌`)
+      });
+   });
+}
+
+const _rm = async (rawTxt) => {
    let txt = rawTxt;
    // remove /n
    txt = await txt.replace(/(\r\n|\r|\n)+/gi, ' ');
@@ -80,10 +173,61 @@ const rm = async (rawTxt) => {
    txt = await txt.replace (/&amp;/gi, '&');
    // add a space between emojis
    txt = Emoji.replace(txt, (emoji) => {
-      return ` ${emoji.key} `;
+      return ` ${emoji.emoji} `;
    }, true);
    // replace consecutive white spaces
    txt = txt.replace(/[^\S\r\n]{2,}/, ' ');
    // txt = await txt.trim();
    return txt;
+}
+
+const _run = async (data = {'': 1}) => {
+   let input = {};
+   for (let token of data.split(" ")) {
+      input[token] = 1;
+   }
+   return net.run(input);
+}
+
+// FIXME : create main utils
+const _mostPresentEmotion = (result, min = .5) => {
+   let predominant = "neutral"; //no-emotion available
+   let recognised = {};
+   let predominantWeight = 0.0;
+
+   for (let key of Object.keys(result)) {
+      let current = parseFloat(result[key]);
+      current = Math.round(current * 100) / 100;
+      if (current >= min) {
+         recognised[key] = current;
+         if (current > predominantWeight) {
+            predominantWeight = current;
+            predominant = key;
+         }
+      }
+   }
+
+   // sort resulting object
+   let sortable = [];
+   for (let emotion in recognised) {
+      sortable.push([emotion,recognised[emotion]]);
+   }
+   sortable.sort(function(a, b) {
+      return a[1] - b[1];
+   });
+   sortable = sortable.reverse();
+
+   let sumWeight = sortable.map((e) => e[1]).reduce((a, b) => a + b, 0);
+   for (let s of sortable) {
+      let percent = parseFloat((s[1]/sumWeight).toFixed(2));
+      s.push(percent)
+   }
+
+   return {
+      recognisedEmotions: sortable,
+      predominant: {
+         emotion: predominant,
+         weight: predominantWeight
+      }
+   };
 }
--- src/nlp-utils/ml-emotion-analysis/test.js
+++ src/nlp-utils/ml-emotion-analysis/test.js
@@ -1,9 +1,25 @@
-import {classification} from "./ml-emotion-analysis.mjs";
-const lang = `en`;
-const txt = `❤️ :) Usually, one has to grab ¶ an image from a server (either localhost or a remote one) ¶ in order to get past $ the SameOrigin policy. Error checking is minimal - ¶ i.e, you can select a text/video/whatever file and ¶ the code will still attempt $ to load it and set its ¶ contents to be the data used to $ construct a dataURI ¶ for an image element.`;
+import dotenv from 'dotenv';
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-classification(txt, lang).then((res) => {
-    console.log(`res=${res}`);
-}).catch((err) => {
-    console.error(err);
-})
+dotenv.config({
+    debug: true,
+    path: `${__dirname}/../.env`
+});
+
+
+import {config, classification} from "./ml-emotion-analysis.mjs";
+const lang = `pt`;
+// const txt = `❤️ :) Usually, one has to grab ¶ an image from a server (either localhost or a remote one) ¶ in order to get past $ the SameOrigin policy. Error checking is minimal - ¶ i.e, you can select a text/video/whatever file and ¶ the code will still attempt $ to load it and set its ¶ contents to be the data used to $ construct a dataURI ¶ for an image element.`;
+const txt = ":) As saudades que eu já tenho da minha alegre casinha ❤️";
+
+config(process.env.LANGUAGE_TRANSLATOR_IAM_APIKEY, process.env.LANGUAGE_TRANSLATOR_URL).then((res) => {
+    classification(txt, lang).then((res) => {
+        console.log(`res=${JSON.stringify(res)}`);
+    }).catch((err) => {
+        console.error(err);
+    })
+});
+
