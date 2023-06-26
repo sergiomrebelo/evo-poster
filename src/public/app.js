8891b841839829dedc5205928bfe85ef866f7347
Sérgio Rebelo <sergiorebelo@me.com>
2023-06-26 13:56:53 +0200

    feat: integrate API request in new scheme #22

--- src/public/app.js
+++ src/public/app.js
@@ -1,16 +1,14 @@
-import 'p5';
-
-import 'bootstrap/dist/css/bootstrap.css';
-// import './main.css';
-
-
-
 import {LitElement, html, css} from "lit";
-
 import {InputForm} from "./components/InputForm.js";
 import {ResultsContainer} from "./components/ResultsContainer.js";
 
-window.preload = () => {}
+import 'p5';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './main.css';
+
+
+window.preload = () => {
+}
 
 window.setup = () => {
     window.app = document.createElement(`app-evo`); // create app
@@ -22,15 +20,15 @@ window.setup = () => {
 }
 
 window.draw = () => {
-    if (window.app.screen === 0) return;
+    if (window.app.screen === 0) return null;
 }
 
 window.windowResized = () => {
-    if (window.app.screen === 0) return;
+    if (window.app.screen === 0) return null;
 }
 
 
-export class App extends LitElement{
+export class App extends LitElement {
     // App.properties.availableLanguages
     static properties = {
         availableLanguages: [
@@ -40,43 +38,11 @@ export class App extends LitElement{
             'nb', 'pl', 'pt', 'ro', 'ru', 'si', 'sk', 'sl', 'es', 'sv',
             'ta', 'te', 'th', 'tr', 'uk', 'ur', 'vi', 'cy'
         ],
+        imageMaxSize: 1024 // in kb
     }
 
     constructor() {
         super();
-        this.screen = 0;
-    }
-
-    render () {
-        return html`
-            <h1 class="mb-3">Evolving Posters</h1>
-            <input-form></input-form>
-        `;
-    }
-}
-customElements.define('app-evo', App);
-
-
-
-
-
-
-/*import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './main.css';
-
-import '../../node_modules/p5/lib/p5.min.js';
-
-import {resultsContainer, inputForm} from './components/input-module.js';
-import errorHandler from "./components/errHandler.js";
-import {container} from "./components/utils.js";
-
-// import {setup, preload, draw, windowResized} from "./components/sketch.js";
-// import p5 from "./components/evolution-module.js"
-
-let app;
-
-export default class App {
-    constructor()  {
         this.images = {
             "imageRandomPlacement": true,
             "hasImages": false,
@@ -84,217 +50,43 @@ export default class App {
             "amount": 0,
             "loading": false,
             "randomPlacement": false
-        };
+        }
         this.text = null;
-        this.screen = 0;
-        this.testingVarWatch2 = "testing string";
-
         this.results = null;
+        this.screen = 0;
 
-        // params
-        this.IMAGE_SIZE = 1024; // in kb
-        this.BACKGROUND_COLOR = color(random(255), random(255), random(255));
+        this._inputForm = new InputForm(this.analyse);
     }
 
-    evolve = () => {
-        this.screen = 1;
-        document.getElementById(`input-module`).style.display = "none";
-        document.querySelector(`.p5Canvas`).style.display = "block";
+    analyse = async () => {
+        // get data from input form
+        //  console.log("outside", this._inputForm.get());
+        const formData = this._inputForm.data();
+        console.log(formData);
+        let params = formData.shouldDivide ? `text` : `lines/${formData.delimiter}`;
+        let req = `/${params}/${formData.lang}/${formData.textContent}`;
+        console.log("req=", req);
 
-        // init canvas
-        createCanvas(windowWidth, windowHeight);
-        background (this.BACKGROUND_COLOR);
-
-        window.draw = () => {
-            background (this.BACKGROUND_COLOR);
-            push();
-            fill(0);
-            textSize(36);
-            let leading = 36*1.35;
-            let y = Math.round(leading * this.results.sentences.length/2);
-            for (let i=0; i<this.results.sentences.length; i++) {
-                let sentence =  this.results.sentences[i];
-                text(
-                    sentence,
-                    windowWidth/2-textWidth(sentence)/2,
-                    windowHeight/2 - y
-                );
-                y -= leading;
-            }
-            pop();
-        }
-
-        window.windowResized = () => {
-            clearTimeout(this.resize);
-            this.resize = setTimeout(() => {
-                this.BACKGROUND_COLOR = color(random(255), random(255), random(255));
-                resizeCanvas(windowWidth, windowHeight);
-            }, 100);
-        }
-    }
-
-
-
-    init = () => {
-        const resultsScreen = resultsContainer();
-        const formInput = inputForm();
-
-        const inputModuleContainer = container(`div`, [],`input-module`);
-        inputModuleContainer.appendChild(resultsScreen);
-        inputModuleContainer.appendChild(formInput);
-
-        document.body.appendChild(inputModuleContainer);
-        formInput.addEventListener("submit", this.get);
-        document.getElementById('formControlImages').addEventListener('change', this._uploadImages);
-    }
-
-
-    get = async (e) => {
-        e.preventDefault();
-        let textArea = encodeURIComponent(document.getElementById('formControlTextarea').value);
-        const shouldDivide = document.getElementById('lineDivisionCheck').checked;
-        const lang = encodeURIComponent(document.getElementById('formControlLang').value);
-
-        let handler = `text`;
-        let delimiter;
-        if (!shouldDivide) {
-            delimiter = encodeURIComponent(document.getElementById('formControlTextDelimiter').value);
-            handler = `lines/${delimiter}`;
-        }
-
-        if (this.images.hasImages && !this.images.randomPlacement && !shouldDivide) {
-            let imageDelimiter = encodeURIComponent(document.getElementById('formControlImagePlaceholderDelimiter').value);
-            this.images.nAnchorPoints = [...textArea.matchAll(new RegExp(imageDelimiter,'g'))].length;
-            textArea = textArea.replaceAll(imageDelimiter, `${delimiter}${imageDelimiter}${delimiter}`);
-            if (this.images.nAnchorPoints !== app.images.amount) {
-                const relation = app.images.nAnchorPoints > app.images.amount ? 'higher' : 'smaller';
-                errorHandler({ message: `the amount of image anchor points is ${relation} than the amount of upload images. (anchors:${app.images.nAnchorPoints} / images:${app.images.amount})`});
-            }
-        }
-
-        const url = `/${handler}/${lang}/${textArea}`;
-
-        fetch(url).then((response) => response.json()).then((result) => {
-            this._displayResults(result);
-            this.results = result;
-        }).catch((error) => {
-            console.error('Error:', error);
-            errorHandler ({ message: `error on fetch. ${error}`});
-        });
-    }
-
-    _displayResults = async (res) => {
-        if (!res.success)  {
-            errorHandler(res.err);
-        }
-
-        document.getElementById('temp-res-text').textContent = `${res.text} (${res.lang})`;
-        document.getElementById('temp-res-sentences').textContent = `${res.sentences.flat()} (${res.sentences.flat().length})`;
-        document.getElementById('temp-res-classification').textContent = `${res.classification.emotions.data.predominant.emotion} (${res.classification.emotions.data.predominant.weight})`;
-        const el = document.getElementById('temp-res-lexicon-lines');
-        el.innerHTML = '';
-        for (let i in res.lexicon.sentences) {
-            const sentence = res.lexicon.sentences[i]
-            const span = document.createElement("span");
-            span.innerHTML = `[${i}] ${sentence.text} (${sentence.emotions.data.predominant.emotion}, ${sentence.emotions.data.predominant.weight}) <br>`;
-            el.appendChild(span);
-        }
-
-        document.getElementById('temp-res-lexicon-global').innerHTML = `<b>global lexicon</b>: ${res.lexicon.global[0][0]} (${res.lexicon.global[0][1]})`
-        document.getElementById('temp-info').classList.replace('d-none', 'd-block');
-        document.querySelector('#input-form fieldset').disabled = true;
-        document.getElementById('btReload').enable = true;
-
-        const section = document.getElementById(`info-results-section`);
-        section.classList.replace('d-none', 'd-block');
-
-        const bts =  document.querySelectorAll(`.nextBts`);
-        bts.forEach((el) => {
-            el.disabled = false;
-            el.classList.replace('d-none', 'd-inline');
-        });
-
-        document.getElementById(`btNext`).onclick = (e) => {
-            e.preventDefault();
-            if (this.results !== null) {
-                this.evolve();
-            } else {
-                errorHandler({ message: "text input not defined"})
-            }
-        }
-
-    }
-
-    _uploadImages = async (e) => {
-        this.images.blobs = [];
-        this.images.hasImages = true;
-        this.images.loading = true;
-        this.images.amount = e.target.files.length;
-
-       this.images.blobs = await this._readImages(e.target.files).catch((err) => {
-           errorHandler ({ message: `not possible to load the image ${err}`});
+        fetch(req).then((response) => response.json()).then((res) => {
+           console.log(res);
+           // TODO: display results
+           this.results = res;
+        }).catch((err) => {
+            console.error(err);
+            //TODO: error handler
         })
-
-        this.images.loading = false;
-        this._displayImages(this.images.blobs);
-
     }
 
-    _displayImages = (files) => {
-        const imgContainer = document.getElementById('input-images');
-        imgContainer.innerHTML = ``;
-
-        for (let i = 0; i<files.length; i++) {
-            const img = new Image();
-            img.src = files[i];
-            img.classList.add('d-inline-block', `mb-2`, `mr-2`);
-            img.style.height = `100px`;
-            img.style.width = `auto`;
-            imgContainer.appendChild(img);
-        }
-        imgContainer.classList.replace('d-none', 'd-block');
-        document.getElementById("input-images-headline").classList.replace('d-none', 'd-block');
+    render() {
+        return html`
+            <h1 class="mb-3">Evolving Posters</h1>
+            <div id="input-module">
+                <results-container></results-container>
+                ${this._inputForm}
+            </div>
+        `;
     }
+}
 
-    _readImages = async (files) => {
-        const res = [];
-        let err = [];
-        const getBase64 = (file) => {
-            const reader = new FileReader()
-            return new Promise(resolve => {
-                reader.onload = (ev) => {
-                    resolve(ev.target.result)
-                }
-                reader.readAsDataURL(file);
-            });
-        };
-
-        for (let i = 0; i < files.length; i++) {
-            if (files[i].size/1024 < this.IMAGE_SIZE) {
-                // if (files[i].size)
-                if (files[i].type.includes('image')) {
-                    res.push(getBase64(files[i]));
-                } else {
-                    err.push(`error loading the following image(s): ${files[i].name}.`);
-                }
-            } else {
-                err.push(`${files[i].name} size bigger than ${this.IMAGE_SIZE} kb.`);
-            }
-        }
-
-        if (err.length > 0) {
-            let msg = "";
-            for (let i=0; i<err.length; i++) {
-                msg += err[i];
-                if (i !== err.length-1) {
-                    msg += "<br>";
-                }
-            }
-
-            errorHandler({message: msg});
-        }
-
-        return await Promise.all(res);
-    }
-}*/
+customElements.define('app-evo', App);
 
--- src/public/components/InputForm.js
+++ src/public/components/InputForm.js
@@ -3,18 +3,31 @@ import {App} from "../app.js";
 import {Divider} from "./Divider.js";
 
 export class InputForm extends LitElement {
-    constructor() {
+    constructor(onSubmit) {
         super();
+        this._onSubmit = async (e) => {
+            e.preventDefault();
+            await onSubmit();
+        }
     }
 
     _toggleVisibility = (e) => {
         const related = e.target.dataset.related;
         const el = this.shadowRoot.getElementById(related);
 
-       if(e.target.checked && el !== null) {
-           el.classList.add('d-none');
+        if (e.target.checked && el !== null) {
+            el.classList.add('d-none');
         } else {
-           el.classList.remove('d-none');
+            el.classList.remove('d-none');
+        }
+    }
+
+    data = () => {
+        return {
+            "textContent": encodeURIComponent(this.shadowRoot.getElementById('formControlTextarea').value),
+            "shouldDivide": this.shadowRoot.getElementById('lineDivisionCheck').checked,
+            "lang": encodeURIComponent(this.shadowRoot.getElementById('formControlLang').value),
+            "delimiter": encodeURIComponent(this.shadowRoot.getElementById('formControlTextDelimiter').value)
         }
     }
 
@@ -22,24 +35,25 @@ export class InputForm extends LitElement {
         return html`
             <div class="row">
                 <section class="input-form-outer col-10 offset-1 mt-5">
-                    <form id="input-form" class="input-form-inner">
+                    <form id="input-form" class="input-form-inner" @submit=${this._onSubmit}>
                         <fieldset>
                             <div class="form-group row mb-2">
                                 <label for="formControlTextarea" class="col-sm-2 col-form-label-sm">Input Text</label>
-                                <textarea id="formControlTextarea" class="form-control col-sm-10" required rows=6></textarea>
+                                <textarea id="formControlTextarea" class="form-control col-sm-10" required
+                                          rows=6></textarea>
                             </div>
                             <div class="form-group row mb-2">
                                 <label for="formControlLang">Language</label>
-                                <select class="form-control form-control-lg" id="formControlLang" type="text" >
+                                <select class="form-control form-control-lg" id="formControlLang" type="text">
                                     ${App.properties.availableLanguages.map((lang) => {
-                                        let  opt = html`
-                                                <option value=${lang}>${lang}</option>`;
+                                        let opt = html`
+                                            <option value=${lang}>${lang}</option>`;
                                         if (lang === 'en') {
                                             opt = html`
                                                 <option selected=true value=${lang}>${lang}</option>`;
                                         }
                                         return opt;
-                                        })
+                                    })
                                     }
                                 </select>
                             </div>
@@ -48,29 +62,35 @@ export class InputForm extends LitElement {
                                 <label for="lineDivisionCheck" class="form-check-label col-form-label-sm">
                                     Automatic line division
                                 </label>
-                                <input 
+                                <input
                                         type="checkbox" @change="${this._toggleVisibility}"
-                                        class="form-check-input" value="1" id="lineDivisionCheck" data-related="textDelimiterField" checked>
+                                        class="form-check-input" value="1" id="lineDivisionCheck"
+                                        data-related="textDelimiterField" checked>
                             </div>
                             <div class="form-group row mb-2 d-none" id="textDelimiterField">
-                                <label for="formControlTextDelimiter" class="col-form-label-sm">Text Line delimiter</label>
-                                <input type="text" value="¶" class="form-control form-control-lg d-none" id="formControlTextDelimiter">
+                                <label for="formControlTextDelimiter" class="col-form-label-sm">Text Line
+                                    delimiter</label>
+                                <input type="text" value="¶" class="form-control form-control-lg d-none"
+                                       id="formControlTextDelimiter">
                             </div>
                             ${Divider.get()}
                             <div class="form-group row mb-2">
                                 <label for="formControlImages" class="col-form-label-sm">Images</label>
-                                <input type="file" class="form-control-file" id="formControlImages" 
+                                <input type="file" class="form-control-file" id="formControlImages"
                                        checked="files[]" accept="image/jpeg, image/png, image/jpg" multiple>
                             </div>
                             <div class="form-check form-check-inline mb-2 d-none" id="imagePlacementField">
-                                <label for="form-check-label, col-form-label-sm" id="imagePlacementCheck">Image Random Placement</label>
+                                <label for="form-check-label, col-form-label-sm" id="imagePlacementCheck">Image Random
+                                    Placement</label>
                                 <input type="checkbox" value="1" @change="${this._toggleVisibility}"
-                                       class="form-check-input" id="imagePlacementCheck"  data-related="imageAnchorField"
+                                       class="form-check-input" id="imagePlacementCheck" data-related="imageAnchorField"
                                        checked>
                             </div>
                             <div class="form-group row mb-2 d-none" id="imageAnchorField">
-                                <label for="formControlImagePlaceholderDelimiter" class="col-form-label-sm">Image Placement Anchor</label>
-                                <input type="text" value=${null} class="form-control form-control-lg" id="formControlImagePlaceholderDelimiter" checked=${false}>
+                                <label for="formControlImagePlaceholderDelimiter" class="col-form-label-sm">Image
+                                    Placement Anchor</label>
+                                <input type="text" value=${null} class="form-control form-control-lg"
+                                       id="formControlImagePlaceholderDelimiter" checked=${false}>
                             </div>
                             ${Divider.get()}
                             <div class="col-auto">
@@ -78,10 +98,12 @@ export class InputForm extends LitElement {
                             </div>
                         </fieldset>
                     </form>
-                    <button type="button" id="btReload" @click ="${() => {window.location.reload()}}"
+                    <button type="button" id="btReload" @click="${() => {
+                        window.location.reload()
+                    }}"
                             class="btn d-none btn-secondary my-2 nextBts" disabled>New Analysis
                     </button>
-                    <button type="button" 
+                    <button type="button"
                             class="btn d-none btn-primary my-2 nextBts mx-3" disabled>Next
                     </button>
                 </section>
