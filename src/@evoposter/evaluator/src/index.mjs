6734435c1e6d08176b97d46ed1d90d258116d922
Sérgio Rebelo <sergiorebelo@me.com>
2023-07-13 02:19:00 +0200

    feat: init evaluation module #25

---
+++ src/@evoposter/evaluator/src/index.mjs
@@ -0,0 +1,14 @@
+/**
+ * Evaluation metrics for evo-poster
+ * Sérgio M. Rebelo
+ * CDV lab. (CMS, CISUC, Portugal)
+ * srebelo[at]dei.uc.pt
+ *
+ * v0.0.1 July 2023
+ */
+
+export const info = () => {
+    console.log ("Evaluator working");
+}
+
+export { info as default };
