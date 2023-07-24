edabdb189ea1ba4dce97f9ccfddb1db0ba776320
Sérgio Rebelo <sergiorebelo@me.com>
2023-07-24 20:32:04 +0200

    refactor: Tournament based on ranking #46 #52
    Since I need to made modification on tournament method, I took that opportunity to modify the tournament based on the meetings feedback

--- src/client/utils.js
+++ src/client/utils.js
@@ -10,6 +10,30 @@ export const swap = (arr, i, j) => {
     return arr;
 }
 
+export const sumArr = (arr) => {
+    return arr.reduce((partialSum, a) => partialSum + a, 0);
+}
+
+export const sus = (probs, max=1) => {
+    const r = Math.random()*max;
+    let c = 0;
+    for (let i in probs) {
+        c += probs[i];
+        if (r < c) {
+            return i;
+        }
+    }
+
+}
+
+export const shuffleArr = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
+
 export default () => {
     console.log (`default export utils`);
 }
